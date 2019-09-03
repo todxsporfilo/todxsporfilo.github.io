b3dfcf8628ba930a1124beed66bf2005e27b32c2
todxsporfilo <54824294+todxsporfilo@users.noreply.github.com>
2019-09-02 22:17:56 -0300

    Add files via upload

---
+++ datos.py
@@ -0,0 +1,191 @@
+# CODIGOS DE LAS MATERIAS
+codigos_materias = dict()
+codigos_materias["antigua"] = "Historia de la Filosofía Antigua"
+codigos_materias["medieval"] = "Historia de la Filosofía Medieval"
+codigos_materias["moderna"] = "Historia de la Filosofía Moderna"
+codigos_materias["logica"] = "Lógica"
+codigos_materias["pensamiento"] = "Pensamiento Argentino y Latinoamericano"
+codigos_materias["gnoseologia"] = "Gnoseología"
+codigos_materias["contemporanea"] = "Filosofía Contemporánea"
+codigos_materias["etica"] = "Ética"
+codigos_materias["metafisica"] = "Metafísica"
+codigos_materias["ciencias"] = "Filosofía de las Ciencias"
+
+tramo_basico = ("antigua", "medieval", "moderna", "logica", "pensamiento", "gnoseologia", "contemporanea",
+                "etica", "metafisica", "ciencias")
+
+codigos_materias["fundamentos"] = "Fundamentos de Filosofía"
+codigos_materias["lenguaje"] = "Filosofía del Lenguaje"
+codigos_materias["antropo_filo"] = "Antropología Filosófica"
+codigos_materias["estetica"] = "Estética"
+codigos_materias["lcg1"] = "Lengua y cultura griegas I o Griego Filosófico I"
+codigos_materias["lcg2"] = "Lengua y cultura griegas II o Griego Filosófico II"
+codigos_materias["lcl1"] = "Lengua y cultura latinas I o Latín Filosófico I"
+codigos_materias["lcl2"] = "Lengua y cultura latinas II o Latín Filosófico II"
+codigos_materias["gramatica"] = "Gramática"
+codigos_materias["linguistica"] = "Lingüística"
+codigos_materias["tyal"] = "Teoría y Análisis Literario"
+codigos_materias["hta"] = "Historia de la Teoría Antropológica"
+codigos_materias["antropo_siste"] = "Antropología Sistemática I (Organización social y política)"
+codigos_materias["filo_educacion"] = "Filosofía de la educación (Licenciatura)"
+codigos_materias["hsg"] = "Historia Social General"
+codigos_materias["hsp"] = "Historia de los Sistemas Políticas"
+codigos_materias["hse"] = "Historia de los Sistemas Económicos"
+
+tramo_electivo = ("fundamentos", "lenguaje", "antropo_filo", "estetica", "lcg1", "lcg2", "lcl1", "lcl2",
+                  "gramatica", "linguistica", "tyal", "hta", "antropo_siste", "filo_educacion",
+                  "hsg", "hsp", "hse")
+
+codigos_materias["pe_antigua"] = "Problemas Especiales de Filosofía Antigua"
+codigos_materias["pe_medieval"] = "Problemas Especiales de Filosofía Medieval"
+codigos_materias["pe_moderna"] = "Problemas Especiales de Filosofía Moderna"
+codigos_materias["pe_contemporanea"] = "Problemas Especiales de Filosofía Contemporánea"
+codigos_materias["pe_pensamiento"] = "Problemas Especiales de Pensamiento Argentino y Latinoamericano"
+
+orientacion_tradicion = ("pe_antigua", "pe_medieval", "pe_moderna", "pe_contemporanea", "pe_pensamiento",
+                         "lcg1", "lcg2", "lcl1", "lcl2")
+
+codigos_materias["pe_logica"] = "Lógica superior"
+codigos_materias["hist_cs"] = "Historia de la ciencia"
+codigos_materias["pe_ciencias"] = "Filosofía Especial de las Ciencias"
+codigos_materias["pe_lenguaje"] = "Problemas Especiales de Filosofía del Lenguaje"
+codigos_materias["pe_antropo"] = "Problemas Especiales de Antropología Filosófica"
+codigos_materias["pe_metafisica"] = "Problemas Especiales de Metafísica"
+codigos_materias["pe_gnoseologia"] = "Problemas especiales de Gnoseología"
+
+orientacion_teorica = ("pe_logica", "hist_cs", "pe_ciencias", "lenguaje", "pe_lenguaje", "antropo_filo",
+                       "pe_antropo", "pe_metafisica", "pe_gnoseologia")
+
+codigos_materias["filo_historia"] = "Filosofía de la Historia"
+codigos_materias["pe_filo_historia"] = "Problemas Especiales de Filosofía de la Historia"
+codigos_materias["pe_estetica"] = "Problemas Especiales de Estética"
+codigos_materias["derecho"] = "Filosofía del Derecho"
+codigos_materias["pe_derecho"] = "Problemas Especiales de Filosofía del Derecho"
+codigos_materias["pe_politica"] = "Problemas Especiales de Filosofía Política"
+codigos_materias["pe_etica"] = "Problemas Especiales de Ética"
+
+orientacion_practica = ("filo_historia", "pe_filo_historia", "estetica", "pe_estetica", "derecho", "pe_derecho",
+                        "pe_politica", "pe_etica")
+
+orientacion = ("pe_antigua", "pe_medieval", "pe_moderna", "pe_contemporanea", "pe_pensamiento",
+               "lcg1", "lcg2", "lcl1", "lcl2", "pe_logica", "hist_cs", "pe_ciencias", "lenguaje", "pe_lenguaje",
+               "antropo_filo", "pe_antropo", "pe_metafisica", "pe_gnoseologia", "filo_historia",
+               "pe_filo_historia", "estetica", "pe_estetica", "derecho", "pe_derecho", "pe_politica", "pe_etica")
+
+codigos_materias["seminario1"] = "Seminario1"
+codigos_materias["seminario2"] = "Seminario1"
+codigos_materias["seminario3"] = "Seminario1"
+codigos_materias["seminario_mat_4"] = "Seminario 4 o Materia Orientación (no elegida previamente) o Materia Optativa (no básica) o materia de otras carreras"
+codigos_materias["seminario_mat_5"] = "Seminario 5 o Materia Orientación (no elegida previamente) o Materia Optativa (no básica) o materia de otras carreras"
+codigos_materias["mat_6"] = "Materia Orientación (no elegida previamente) o Materia Optativa (no básica) o materia de otras carreras"
+
+seminarios = ("seminario1", "seminario2", "seminario3", "seminario_mat_4", "seminario_mat_5", "mat_6")
+
+codigos_materias["hsg_edu"] = "Historia Social General de la Educación"
+codigos_materias["hel"] = "Historia de la Educación Latinoamericana"
+codigos_materias["aie"] = "Análisis Institucional de la Escuela"
+codigos_materias["socio_ed"] = "Sociología de la educación"
+codigos_materias["hea"] = "Historia de la Educación Argentina"
+codigos_materias["politica_educ"] = "Política Educacional"
+
+electivas_profesorado = ("filo_educacion", "hsg_edu", "hel", "aie", "socio_ed", "hea", "politica_educ")
+
+codigos_materias["didact_gen"] = "Didáctica General"
+codigos_materias["didact_esp"] = "Didáctica Especial y Práctica de la Enseñanza (tenés que haber cursado Teoría y Práctica de la enseñanza filosófica o Psicología general)"
+codigos_materias["typef"] = "Teoría y Práctica de la enseñanza filosófica"
+
+orientacion_profesorado = ("didact_gen", "didact_esp", "typef")
+
+codigos_materias["ingles1"] = "Ingles 1"
+codigos_materias["ingles2"] = "Ingles 1"
+codigos_materias["ingles3"] = "Ingles 1"
+codigos_materias["aleman1"] = "Alemán 1"
+codigos_materias["aleman2"] = "Alemán 2"
+codigos_materias["aleman3"] = "Alemán 3"
+codigos_materias["frances1"] = "Francés 1"
+codigos_materias["frances2"] = "Francés 2"
+codigos_materias["frances3"] = "Francés 3"
+codigos_materias["italiano1"] = "Italiano 1"
+codigos_materias["italiano2"] = "Italiano 2"
+codigos_materias["italiano3"] = "Italiano 3"
+codigos_materias["portugues1"] = "Portugués 1"
+codigos_materias["portugues2"] = "Portugués 2"
+codigos_materias["portugues3"] = "Portugués 3"
+
+codigos_materias["tesis"] = "Tesis de licenciatura"
+
+idiomas_sajones = ("ingles1", "ingles2", "ingles3", "aleman1", "aleman2", "aleman3")
+idiomas_latinos = ("frances1", "frances2", "frances3", "italiano1", "italiano2", "italiano3",
+                   "portugues1", "portugues2", "portugues3")
+idiomas = ("ingles1", "ingles2", "ingles3", "aleman1", "aleman2", "aleman3", "frances1", "frances2", "frances3",
+           "italiano1", "italiano2", "italiano3", "portugues1", "portugues2", "portugues3")
+
+
+# --------------------------------------------------------------------------------------------------------------
+# CORRELATIVIDADES / REQUISITOS
+
+requeridas = dict()
+requeridas["antigua"] = set()
+requeridas["logica"] = set()
+requeridas["etica"] = set()
+requeridas["pensamiento"] = set()
+requeridas["fundamentos"] = set()
+
+for i in ("aleman", "ingles", "frances", "portugues", "italiano"):
+    requeridas[i+"1"] = set()
+    requeridas[i+"2"] = {i+"1"}
+    requeridas[i+"3"] = {i+"2", i+"1"}
+
+requeridas["ciencias"] = {"logica",  "moderna"}
+requeridas["pe_ciencias"] = {"ciencias"}
+requeridas["politica"] = {"moderna", "gnoseologia", "etica"}
+requeridas["metafisica"] = {"moderna"}
+requeridas["medieval"] = {"antigua"}
+requeridas["lcg1"] = {"antigua"}
+requeridas["moderna"] = {"medieval"}
+requeridas["lcl1"] = {"medieval"}
+requeridas["contemporanea"] = {"moderna"}
+requeridas["gnoseologia"] = {"moderna"}
+requeridas["lenguaje"] = {"moderna", "logica", "gnoseologia"}
+requeridas["antropo_filo"] = {"moderna","gnoseologia","metafisica"}
+requeridas["estetica"] = {"moderna", "gnoseologia", "metafisica"}
+requeridas["lcg2"] = {"lcg1"}
+requeridas["lcl2"] = {"lcl1"}
+requeridas["lcg1"] = {"antigua"}
+requeridas["lcl1"] = {"medieval"}
+requeridas["linguistica"] = {"gramatica"}
+requeridas["pe_antigua"] = {"antigua", "lcg1"}
+requeridas["pe_medieval"] = {"medieval", "lcl1"}
+requeridas["pe_moderna"] = {"moderna"}
+requeridas["pe_contemporanea"] = {"contemporanea", "metafisica",  "gnoseología"}
+requeridas["pe_pensamiento"] = {"pensamiento", "moderna"}
+requeridas["pe_logica"] = {"logica"}
+requeridas["hist_cs"] = {"ciencias"}
+requeridas["pe_lenguaje"] = {"lenguaje", "gnoseología", "metafísica"}
+requeridas["pe_metafisica"] = {"gnoseologia", "metafisica", "contemporanea"}
+requeridas["pe_gnoseologia"] = {"gnoseologia", "metafisica", "contemporanea"}
+requeridas["pe_filo_historia"] = {"gnoseologia", "metafisica", "filo_historia"}
+requeridas["filo_historia"] = {"gnoseologia", "metafisica", "contemporanea"}
+requeridas["pe_estetica"] = {"estetica", "metafisica", "gnoseologia"}
+requeridas["derecho"] = {"politica", "etica"}
+requeridas["pe_derecho"] = {"derecho"}
+requeridas["pe_politica"] = {"politica"}
+requeridas["pe_etica"] = {"etica"}
+requeridas["pe_antropo"] = {"antropo_filo"}
+
+requeridas["seminario1"] = set()
+requeridas["seminario2"] = {"seminario1"}
+requeridas["seminario3"] = {"seminario2"}
+requeridas["seminario_mat_4"] = set()
+requeridas["seminario_mat_5"] = set()
+requeridas["mat_6"] = set()
+
+requeridas["didact_gen"] = set()
+requeridas["typef"] = set()
+requeridas["didact_esp"] = {"didact_gen"}
+
+
+# MATERIAS PARA LAS CUALES NO SE LOS REQUERIMIENTOS
+for x in ('gramatica', 'tyal', 'hta', 'antropo_siste', 'filo_educacion', 'hsg', 'hsp', 'hse', 'hsg_edu',
+          'hel', 'aie', 'socio_ed', 'hea', 'politica_educ', 'tesis'):
+    requeridas[x] = set()
